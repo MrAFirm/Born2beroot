a7d9802d7d0c9bb0a0daaf6fabf1807b49bcb120
Shaun Liew Khye Yang <161272658+MrAFirm@users.noreply.github.com>
2024-07-19 02:11:54 +0800

    Update monitoring.sh
    Updated script from Gemartin but with some changes.

--- monitoring.sh
+++ monitoring.sh
@@ -1,34 +1,59 @@
-/*This is the bash script for the mandatory part for the calculations and display of your system info.*/
-
-
+# This is the bash script that is used for displaying certain system information. It's essentially Gemartin's Code but I did type this out by hand and changed the variable names.
 
 #!/bin/bash
-arc=$(uname -a)
-pcpu=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l) 
-vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
-fram=$(free -m | awk '$1 == "Mem:" {print $2}')
-uram=$(free -m | awk '$1 == "Mem:" {print $3}')
-pram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
-fdisk=$(df -BG | grep '^/dev/' | grep -v '/boot$' | awk '{ft += $2} END {print ft}')
-udisk=$(df -BM | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
-pdisk=$(df -BM | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
-cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
-lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
-lvmu=$(if [ $(lsblk | grep "lvm" | wc -l) -eq 0 ]; then echo no; else echo yes; fi)
-ctcp=$(ss -Ht state established | wc -l)
-ulog=$(users | wc -w)
-ip=$(hostname -I)
-mac=$(ip link show | grep "ether" | awk '{print $2}')
-cmds=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
-wall "	#Architecture: $arc
-	#CPU physical: $pcpu
-	#vCPU: $vcpu
-	#Memory Usage: $uram/${fram}MB ($pram%)
-	#Disk Usage: $udisk/${fdisk}Gb ($pdisk%)
-	#CPU load: $cpul
-	#Last boot: $lb
-	#LVM use: $lvmu
-	#Connections TCP: $ctcp ESTABLISHED
-	#User log: $ulog
-	#Network: IP $ip ($mac)
-	#Sudo: $cmds cmd"
+
+# ARCH
+arch=$(uname -a)
+
+# CPU Physical
+pcpu=$(grep "physical id" /proc/cpuinfo | wc -l)
+
+# Virtual CPU
+vcpu=$(grep "processor" /proc/cpuinfo | wc -l)
+
+# RAM
+used_ram=$(free --mega | awk '$1 == "Mem:" {print $3}')
+total_ram=$(free --mega | awk '$1 == "Mem:" {print $2}')
+percentage_ram=$(free --mega | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
+
+# Disk Storage
+used_disk=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{memory_use += $3} END {print memory_use}')
+total_disk=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{memory_total += $2} END {printf ("%.1fGb\n"), memory_total/1024}')
+percentage_disk=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{used += $3} {total+= $2} END {printf("%d"), used/total*100}')
+
+# CPU Load
+cpu_load=$(vmstat 1 4 | tail -1 | awk '{printf $15}')
+cpu_op=$(expr 100 - $cpu_load)
+cpu_fin=$(printf "%.1f" $cpu_op)
+
+# Last Boot
+last_boot=$(who -b | awk '$1 == "system" {print $3 " " $4}')
+
+# LVM Use
+lvm_use=$(if [ $(lsblk | grep "lvm" | wc -l) -gt 0 ]; then echo yes; else echo no; fi)
+
+# TCP Connections
+tcp_connections=$(ss -ta | grep ESTAB | wc -l)
+
+# User Log
+user_log=$(users | wc -w)
+
+# Network
+ip_address=$(hostname -I)
+mac_address=$(ip link | grep "link/ether" | awk '{print $2}')
+
+# SUDO Command
+sudo_command=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+
+wall "	Architecture: $arch
+	CPU physical: $pcpu
+	vCPU: $vcpu
+	Memory Usage: $used_ram/${total_ram}MB ($percentage_ram%)
+	Disk Usage: $used_disk/${total_disk} ($percentage_disk%)
+	CPU load: $cpu_fin%
+	Last boot: $last_boot
+	LVM use: $lvm_use
+	Connections TCP: $tcp_connections ESTABLISHED
+	User log: $user_log
+	Network: IP $ip_address ($mac_address)
+	Sudo: $sudo_command cmd"
